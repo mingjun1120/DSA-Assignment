c24b877b4c91b141aa7f8240a13d5b6576de80a8
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-02-26 21:46:03 +0800

    Update mainClass.java

--- src/main/mainClass.java
+++ src/main/mainClass.java
@@ -17,7 +17,7 @@ public class mainClass {
             int selection;
             switch (operation_selection)
             {
-                case 1: orderOp.addNewOrder(); break;
+                case 1: orderOp.addNewOrder(); orderOp.displayOrder(); break;
                 case 2: do {
                             modifyMenuTable();
                             selection = doSelection(5, "Enter your choice (1-");
