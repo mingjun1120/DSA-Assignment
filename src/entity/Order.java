b5d8c28373fb5eab673ec3fa9a3c2df73894df97
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-01 02:11:16 +0800

    Update Order.java

--- src/entity/Order.java
+++ src/entity/Order.java
@@ -5,45 +5,45 @@ public class Order {
     private String orderID;
     private GregorianCalendar orderTime;
     private double orderTotalPrice;
-    private int quantity;
+    private OrderDish cusOrder;
 
-    public Order(String orderID, GregorianCalendar orderTime, double orderTotalPrice, int quantity) {
+    public Order(String orderID, GregorianCalendar orderTime, double orderTotalPrice, OrderDish cusOrder) {
         this.orderID = orderID;
         this.orderTime = orderTime;
         this.orderTotalPrice = orderTotalPrice;
-        this.quantity = quantity;
+        this.cusOrder = cusOrder;
     }
 
     public String getOrderID() {
         return orderID;
     }
 
-    public GregorianCalendar getOrderTime() {
-        return orderTime;
-    }
-
-    public double getOrderTotalPrice() {
-        return orderTotalPrice;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
     public void setOrderID(String orderID) {
         this.orderID = orderID;
+    }
+
+    public GregorianCalendar getOrderTime() {
+        return orderTime;
     }
 
     public void setOrderTime(GregorianCalendar orderTime) {
         this.orderTime = orderTime;
     }
 
+    public double getOrderTotalPrice() {
+        return orderTotalPrice;
+    }
+
     public void setOrderTotalPrice(double orderTotalPrice) {
         this.orderTotalPrice = orderTotalPrice;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public OrderDish getCusOrder() {
+        return cusOrder;
+    }
+
+    public void setCusOrder(OrderDish cusOrder) {
+        this.cusOrder = cusOrder;
     }
 
     @Override
@@ -52,7 +52,7 @@ public class Order {
                 "orderID='" + orderID + '\'' +
                 ", orderTime=" + orderTime +
                 ", orderTotalPrice=" + orderTotalPrice +
-                ", quantity=" + quantity +
+                ", cusOrder=" + cusOrder +
                 '}';
     }
 }
