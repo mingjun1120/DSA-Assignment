cd5f9023777d2902c0a92e7ff4079b140de80843
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-10 02:05:48 +0800

    Update OrderListInterface.java

--- src/adt/OrderListInterface.java
+++ src/adt/OrderListInterface.java
@@ -1,34 +1,34 @@
-package adt;
-
-public interface OrderListInterface<T> extends ListInterface<T> {
-
-    void offer(T newEntry);
-    /*
-     * Description   - add a type T object into the list
-     * Postcondition - a new type T object will be added into dish list
-     */
-
-    T poll();
-    /*
-     * Description   - Retrieve and remove the T object at head
-     * Precondition  - List is not empty
-     * Postcondition - the T object at the head is removed
-     * Return        - T Object at the head; null otherwise if the list is empty
-     */
-
-    T getFront();
-    /*
-     * Description   - Retrieve the first T object
-     * Precondition  - List is not empty
-     * Postcondition - List remains unchanged
-     * Return        - T Object at the front; null otherwise if the list is empty
-     */
-
-    T getLast();
-    /*
-     * Description   - Retrieve the last T object
-     * Precondition  - List is not empty
-     * Postcondition - List remains unchanged
-     * Return        - T Object at the back; null otherwise if the list is empty
-     */
-}
+//package adt;
+//
+//public interface OrderListInterface<T> extends ListInterface<T> {
+//
+//    void offer(T newEntry);
+//    /*
+//     * Description   - add a type T object into the list
+//     * Postcondition - a new type T object will be added into dish list
+//     */
+//
+//    T poll();
+//    /*
+//     * Description   - Retrieve and remove the T object at head
+//     * Precondition  - List is not empty
+//     * Postcondition - the T object at the head is removed
+//     * Return        - T Object at the head; null otherwise if the list is empty
+//     */
+//
+//    T getFront();
+//    /*
+//     * Description   - Retrieve the first T object
+//     * Precondition  - List is not empty
+//     * Postcondition - List remains unchanged
+//     * Return        - T Object at the front; null otherwise if the list is empty
+//     */
+//
+//    T getLast();
+//    /*
+//     * Description   - Retrieve the last T object
+//     * Precondition  - List is not empty
+//     * Postcondition - List remains unchanged
+//     * Return        - T Object at the back; null otherwise if the list is empty
+//     */
+//}
