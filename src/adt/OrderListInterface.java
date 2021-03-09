d0cb5830a4fcb04f0c53a454406a9a8031727c9e
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-09 20:04:28 +0800

    Update OrderListInterface.java

--- src/adt/OrderListInterface.java
+++ src/adt/OrderListInterface.java
@@ -1,11 +1,19 @@
 package adt;
 
 public interface OrderListInterface<T> extends ListInterface<T> {
+
     void offer(T newEntry);
     /*
      * Description   - add a type T object into the list
-     * Precondition  - dish list is not full
      * Postcondition - a new type T object will be added into dish list
+     */
+
+    T poll();
+    /*
+     * Description   - Retrieve and remove the T object at head
+     * Precondition  - List is not empty
+     * Postcondition - the T object at the head is removed
+     * Return        - T Object at the head; null otherwise if the list is empty
      */
 
     T getFront();
