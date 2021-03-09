cbfacb250c51f4f15cd6b863840249e08c70f12e
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-10 02:05:16 +0800

    Create ArrayListInterface.java

---
+++ src/adt/ArrayListInterface.java
@@ -0,0 +1,18 @@
+package adt;
+
+public interface ArrayListInterface<T> extends ListInterface<T>{
+
+    void doubleSize();
+    /*
+     * Description   - double the size of dish list
+     * Precondition  - dish list remain unchanged
+     * Postcondition - size of dish list become double
+     */
+
+    boolean isFull();
+    /*
+     * Description   - check the list is empty or not
+     * Postcondition - list remain unchanged
+     * Return        - return true if the list is empty; false otherwise
+     */
+}
