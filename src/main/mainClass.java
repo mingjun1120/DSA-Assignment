4c5d7745b2b6764ebf376fc6ffa7c4296c53eb68
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-11 04:04:49 +0800

    Update mainClass.java

--- src/main/mainClass.java
+++ src/main/mainClass.java
@@ -17,42 +17,16 @@ public class mainClass {
             int selection;
             switch (operation_selection)
             {
-                case 1: char ans;
-                        double sum;
-                        int choice;
-                        do {
-                            choice = 0;
-                            orderOp.addNewOrder();
-                            sum = orderOp.displayOrder();
-                            ans = orderOp.addOn_or_modify_confirmation_before_payment();
-                            while (ans == 'Y') {
-                                choice = orderOp.operation();
-                                switch (choice) {
-                                    case 1 -> ans = 'N';
-                                    case 2 -> {
-                                        orderOp.editOrderQty();
-                                        sum = orderOp.displayOrder();
-                                        ans = orderOp.addOn_or_modify_confirmation_before_payment();
-                                    }
-                                    case 3 -> {
-                                        orderOp.removeOrder();
-                                        sum = orderOp.displayOrder();
-                                        ans = orderOp.addOn_or_modify_confirmation_before_payment();
-                                    }
-                                    default ->{
-                                        sum = orderOp.displayOrder();
-                                        ans = orderOp.addOn_or_modify_confirmation_before_payment();
-                                    }
-                                }
-                            }
-                        } while(choice == 1);
-                        if (sum > 0)
-                            orderOp.paymentConfirmation(sum);
+                case 1: do {
+                             orderTable();
+                             selection = doSelection(3, "Enter your choice (1-");
+                             switch_func1(selection);
+                        } while (selection != 3);
                         break;
                 case 2: do {
                             modifyMenuTable();
                             selection = doSelection(5, "Enter your choice (1-");
-                            switch_func(selection);
+                            switch_func2(selection);
                         } while (selection != 5);
                         break;
                 case 3: System.out.println("Hiii!"); break;
@@ -63,8 +37,22 @@ public class mainClass {
     }
 
 
+    private static void switch_func1(int selection) {
+        switch (selection) {
+            case 1: make_order(); break;
+            case 2: orderQueue(); break;
+            default:
+        }
+    }
 
-    private static void switch_func(int modify_selection) {
+    private static void orderQueue() {
+        int loopAgn;
+        do {
+            loopAgn = orderOp.viewOrderQueue();
+        } while (loopAgn == 1);
+    }
+
+    private static void switch_func2(int modify_selection) {
         switch (modify_selection) {
             case 1: addNewDish(); break;
             case 2: editDishName(); break;
@@ -98,6 +86,51 @@ public class mainClass {
         System.out.println("|         4. Remove dish             |");
         System.out.println("|         5. Exit                    |");
         System.out.println("+------------------------------------+");
+    }
+
+    private static void orderTable()
+    {
+        System.out.println("\n+------------------------------------+");
+        System.out.println("|          Order Operation           |");
+        System.out.println("+------------------------------------+");
+        System.out.println("|         1. Make order              |");
+        System.out.println("|         2. View order in queue     |");
+        System.out.println("|         3. Exit                    |");
+        System.out.println("+------------------------------------+");
+    }
+
+    private static void make_order() {
+        char ans;
+        double sum;
+        int choice;
+        do {
+            choice = 0;
+            orderOp.addNewOrder();
+            sum = orderOp.displayOrder();
+            ans = orderOp.addOn_or_modify_confirmation_before_payment();
+            while (ans == 'Y') {
+                choice = orderOp.operation();
+                switch (choice) {
+                    case 1 -> ans = 'N';
+                    case 2 -> {
+                        orderOp.editOrderQty();
+                        sum = orderOp.displayOrder();
+                        ans = orderOp.addOn_or_modify_confirmation_before_payment();
+                    }
+                    case 3 -> {
+                        orderOp.removeOrder();
+                        sum = orderOp.displayOrder();
+                        ans = orderOp.addOn_or_modify_confirmation_before_payment();
+                    }
+                    default ->{
+                        sum = orderOp.displayOrder();
+                        ans = orderOp.addOn_or_modify_confirmation_before_payment();
+                    }
+                }
+            }
+        } while(choice == 1);
+        if (sum > 0)
+            orderOp.paymentConfirmation(sum);
     }
 
     private static void addNewDish() {
