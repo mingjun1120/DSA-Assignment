c0635d7e56d0285810d20cb146dc65356cf7f498
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-15 01:17:45 +0800

    Update StackInterface.java

--- src/adt/StackInterface.java
+++ src/adt/StackInterface.java
@@ -3,46 +3,16 @@ package adt;
 
 
 public interface StackInterface<T> {
-    /**
-     * task: adds a new entry to the top of the stack
-     * @param newEntry an object to be added to the stack
-     */
-    public void push(T newEntry);
-    /**
-     *
-     * task: removes and returns the stack's top entry
-     *
-     * @return either the object at the top of the stack or. if the stack is
-     * empty before the operation null
-     */
 
+    public void push(T newEntry);
 
     public T pop();
 
-    /**
-     * task: retrieves the stack's top entry
-     *
-     * @return either the object at the top of the stack or null if the stack is
-     * empty
-     */
-
     public T peek();
-    /**
-     * task: detects whether the stack is empty
-     *
-     * @return true if the stack is empty
-     */
 
     public boolean isEmpty();
-    /**
-     *
-     * task: removes all entries from the stack
-     */
 
     public void clear();
-    /**
-     * end stackInterface
-     */
 }
     
 
