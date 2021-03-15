4b40c3085b07792a63779227b2f0120a39581932
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-16 03:01:53 +0800

    Update TransHistOperation.java

--- src/main/TransHistOperation.java
+++ src/main/TransHistOperation.java
@@ -8,25 +8,31 @@ import entity.*;
 
 public class TransHistOperation {
     public static Scanner scan = new Scanner(System.in);
-    StackInterface<TransHist> tran_history = new LinkedStack<>();
+    public static StackInterface<TransHist> tran_history = new LinkedStack<>();
 
     public void print_Tran_History() {
         DateTimeFormatter formatter_date = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter formatter_time = DateTimeFormatter.ofPattern("HH:mm:ss a");
 
-        System.out.println("+---------------------------------------------------+");
-        System.out.println("|                TRANSACTION HISTORY                |");
-        System.out.println("+---------------------------------------------------+");
-
+        System.out.println("\n+----------------------------------------------------------------+");
+        System.out.println("|                      TRANSACTION HISTORY                       |");
+        System.out.println("+----------------------------------------------------------------+");
+         System.out.printf("| %-7s  |  %-10s  |  %-11s  | %-8s | %-9s |\n", "TRAN ID", "DATE", "TIME", "ORDER ID", "TOTAL(RM)");
+        System.out.println("+----------|--------------|---------------|----------|-----------+");
         Iterator<TransHist> it = tran_history.getIterator();
         while(it.hasNext()) {
-            System.out.printf("|  %-5s  |  %-10s  |  %-11s  |  %-5s  |  -%5.2f  |\n", it.next().getTranID(),
-                                                     it.next().getTranTime().format(formatter_date),
-                                                     it.next().getTranTime().format(formatter_time),
-                                                     it.next().getTranDetail().getOrderID(),
-                                                     it.next().getTranDetail().getOrderTotalPrice()
+            TransHist th = it.next();
+            System.out.printf("|    %-5s |  %-10s  |  %-11s  |   %-5s  |   %-5.2f   |\n", th.getTranID(),
+                    th.getTranTime().format(formatter_date),
+                    th.getTranTime().format(formatter_time),
+                    th.getTranDetail().getOrderID(),
+                    th.getTranDetail().getOrderTotalPrice()
             );
         }
-        System.out.println(tran_history.isEmpty());
+        System.out.println("+----------------------------------------------------------------+");
+    }
+
+    public static void push(TransHist t){
+        tran_history.push(t);
     }
 }
