d8f7dc7371e2c6b8f80404319bbbfe9293ccedf4
mingjun1120 <54136320+mingjun1120@users.noreply.github.com>
2021-03-16 00:04:15 +0800

    Create LinkedStack.java

---
+++ src/adt/LinkedStack.java
@@ -0,0 +1,110 @@
+package adt;
+import java.util.Iterator;
+
+public class LinkedStack<T> implements StackInterface<T> {
+    private Node topNode;
+
+    public LinkedStack(){
+        topNode = null;
+    }
+
+    @Override
+    public void push(T newEntry) {
+        
+        Node newNode = new Node(newEntry);
+        newNode.next = topNode;
+        topNode = newNode;
+
+        System.out.println("Hi, I got push!!!");
+    }
+
+    @Override
+    public String toString(){
+        String str = "";
+        Node currentNode = topNode;
+        while (currentNode != null){
+            str += currentNode.data + "";
+            currentNode = currentNode.next;
+        }
+        return str;
+    }
+
+    @Override
+    public T pop() {
+        T topEntry = null;
+        if( !isEmpty()){
+            topEntry = topNode.data;
+            topNode = topNode.next;
+        }
+        return topEntry;
+    }
+
+    @Override
+    public T peek() {
+        T  topEntry = null;
+        if( !isEmpty()){
+            topEntry = topNode.data;
+        }
+        return topEntry;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        return topNode == null;
+    }
+
+    @Override
+    public void clear(){
+        topNode = null;
+    }
+
+    @Override
+    public Iterator<T> getIterator() {
+        return new LinkedStackIterator();
+    }
+
+    private class LinkedStackIterator implements Iterator<T> {
+
+        private Node iteratorPointer;
+
+        public LinkedStackIterator() {
+            iteratorPointer = topNode;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iteratorPointer != null;
+        }
+
+        @Override
+        public T next() {
+            T returnData = null;
+            if (hasNext()) {
+                returnData = iteratorPointer.data;
+                iteratorPointer = iteratorPointer.next;
+            }
+            return returnData;
+        }
+
+    }
+
+    private class Node {
+        private T data;
+        private Node next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+
+        public Node(T data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public Node() {
+            
+        }
+    }
+
+
+}
